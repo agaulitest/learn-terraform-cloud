61ca54eb89717e8239e721dbd42528fa6efaf465
agaulitest <126811898+agaulitest@users.noreply.github.com>
2023-03-11 15:54:37 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,29 @@ resource "aws_iam_policy" "inline" {
 EOF
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0975ba0b60f84cee8"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
