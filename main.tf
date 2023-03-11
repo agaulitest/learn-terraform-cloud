48280684d92e579722a6f339ede4927fd357b71d
agaulitest <126811898+agaulitest@users.noreply.github.com>
2023-03-11 15:56:56 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,14 +34,6 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks      = ["10.0.0.0/8"]
   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
   tags = {
     Name = "allow_tls"
   }
