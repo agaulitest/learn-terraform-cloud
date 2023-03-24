82a92fc43eca7ae6272bab98e62eaa9e1557a37e
agaulitest <126811898+agaulitest@users.noreply.github.com>
2023-03-24 08:28:29 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,3 +39,21 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
+resource "aws_instance" "project-tc" {
+  ami = "ami-07f3ef11ec14a1ea3"
+  instance_type = "m5.2xlarge"
+  subnet_id = "subnet-04f5b12a5398896d7"
+  vpc_security_group_ids = [
+    aws_security_group.allow_tls.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    iops = 150
+    volume_size = 50
+    volume_type = "gp2"
+  }
+  tags = {
+  }
+
+}
+
